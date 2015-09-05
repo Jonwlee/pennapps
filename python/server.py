1d52604bb454bb215c0e6fbd60c9b7901191f6da
Ryan Choi <ryanchoi93@gmail.com>
2015-09-05 04:14:31 -0400

    gitignore includes pyc objects (these will be auto generated by flask), server.py updated to refer to analyses.py which handles all the hard work

--- python/server.py
+++ python/server.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, request, abort, jsonify
+import analyses
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Built at PennApps XII 2015! Upload an m4a or amr to /analyze.'
 
-@app.route('/voice')
-def read():
-    return 'Received voice data'
+@app.route('/analyze', methods=['POST'])
+def analyzeVoice():
+    voiceRaw = request.files.get('voiceRaw', None)
+    print(voiceRaw.__dict__)
+    # accept only filetypes that bleh
+    if voiceRaw is None or not voiceRaw.filename.endswith(('.m4a', '.amr')):
+        return 'Invalid filetype or no file uploaded', 400
+    
+    # we've determined that the file is of the correct format. Now, analyze!
+    results = {}
+    results['tone'] = analyses.tone_analyze()
+
+
+    # return results
+    return jsonify(results), 200
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
+
+
 
